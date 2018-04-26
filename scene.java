daab9e66146ab4c4ce8a887bd8bfc48e59cad73d
saadbinsaif <38767594+saadbinsaif@users.noreply.github.com>
2018-04-26 23:07:59 +0300

    Update scene.java
    i changed varible nbsh to nbShape

--- scene.java
+++ scene.java
@@ -2,27 +2,27 @@
 public class scene {
 private String name;
 private shape []arr;
-private int nbsh;
+private int nbShape;
 
 public scene(String name, int size){
 	this.name=name;
  this.arr=new shape[size];
- nbsh=0;
+ nbShape=0;
 }
 public void addshape(shape s){
 	if(nbsh<arr.length){
 		if(s instanceof square)
-			arr[nbsh++]=new square((square)s);
+			arr[nbShape++]=new square((square)s);
 		else
-			arr[nbsh++]=new circle((circle)s);
+			arr[nbShape++]=new circle((circle)s);
 		
 	}
 }
 	public circle[] getcircle (int r){
-		circle[] arrc=new circle[nbsh];
+		circle[] arrc=new circle[nbShape];
 		int a=0;
 		
-			for(int i =0; i<nbsh ;i++){
+			for(int i =0; i<nbShape ;i++){
 				if(arr[i] instanceof circle)
 					if( ((circle)arr[i]).getradius()>r)
 						arrc[a++]=(circle)arr[i];
@@ -44,7 +44,7 @@ public void addshape(shape s){
 	}
 	public void display(){
 		System.out.println("the scene name is :"+name);
-		for(int i =0; i<nbsh ;i++){
+		for(int i =0; i<nbShape ;i++){
 			System.out.println(i+i+"-");
 			arr[i].display();
 			System.out.println("*******************");
